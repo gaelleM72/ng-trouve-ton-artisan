4109fcbf17ef1e021d255c385005c19270929a7c
gaelleM72 <141839974+gaelleM72@users.noreply.github.com>
2024-06-11 18:16:47 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'batiment', component: BatimentComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'fabrication', component: FabricationComponent },
+  { path: 'alimentation', component: AlimentationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
