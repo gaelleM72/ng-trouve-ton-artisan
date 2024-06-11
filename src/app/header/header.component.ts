77b0e23024213de4aaac137c5256350f192b4c53
gaelleM72 <141839974+gaelleM72@users.noreply.github.com>
2024-06-11 17:50:25 +0200

    navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuValue:boolean = false;
+  menu_icon:string='bi bi-list';
 
+  openMenu() {
+    this.menuValue =!this.menuValue;
+    this.menu_icon = this.menuValue ? 'bi bi-x':'bi bi-list';
+  }
+
+  closeMenu() {
+    this.menuValue = false;
+    this.menu_icon = 'bi bi-list';
+  }
 }
