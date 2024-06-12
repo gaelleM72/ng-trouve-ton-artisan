7b501fb80521763081204ab36196541c91ca2b71
gaelleM72 <141839974+gaelleM72@users.noreply.github.com>
2024-06-12 14:52:04 +0200

    routes footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { BatimentComponent } from './batiment/batiment.component';
 import { ServicesComponent } from './services/services.component';
 import { FabricationComponent } from './fabrication/fabrication.component';
 import { AlimentationComponent } from './alimentation/alimentation.component';
+import { MentionsComponent } from './mentions/mentions.component';
+import { DonneesComponent } from './donnees/donnees.component';
+import { AccessibiliteComponent } from './accessibilite/accessibilite.component';
+import { CookiesComponent } from './cookies/cookies.component';
 
 
 const routes: Routes = [
@@ -13,7 +17,11 @@ const routes: Routes = [
   { path: 'batiment', component: BatimentComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'fabrication', component: FabricationComponent },
-  { path: 'alimentation', component: AlimentationComponent }
+  { path: 'alimentation', component: AlimentationComponent },
+  { path: 'mentions', component: MentionsComponent },
+  { path: 'donnees', component: DonneesComponent },
+  { path: 'accessibilite', component: AccessibiliteComponent },
+  { path: 'cookies', component: CookiesComponent }
 ];
 
 @NgModule({
