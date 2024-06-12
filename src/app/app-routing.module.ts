d9f47a19da327393230f1daf7e8f2c0de4337082
gaelleM72 <141839974+gaelleM72@users.noreply.github.com>
2024-06-12 12:27:09 +0200

    validation routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { BatimentComponent } from './batiment/batiment.component';
+import { ServicesComponent } from './services/services.component';
+import { FabricationComponent } from './fabrication/fabrication.component';
+import { AlimentationComponent } from './alimentation/alimentation.component';
+
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'batiment', component: BatimentComponent },
