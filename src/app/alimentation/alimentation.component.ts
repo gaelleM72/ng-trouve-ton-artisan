27c39e78f791d79634fb8ede5f5e93c799740d4d
gaelleM72 <141839974+gaelleM72@users.noreply.github.com>
2024-06-18 17:47:00 +0200

    alimentation

--- src/app/alimentation/alimentation.component.ts
+++ src/app/alimentation/alimentation.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ARTISANS } from '../artisan/artisan-list';
+import { Artisan } from '../artisan/artisans';
 
 @Component({
   selector: 'app-alimentation',
   templateUrl: './alimentation.component.html',
   styleUrl: './alimentation.component.scss'
 })
-export class AlimentationComponent {
+export class AlimentationComponent implements OnInit {
+ 
+  artisanList: Artisan[] = ARTISANS;
+  artisanSelected: Artisan|undefined;
+
+  ngOnInit() {
+    this.artisanList = ARTISANS.filter((Artisan: { specialty:string }) => Artisan.specialty === "Alimentation");
+  }
 
 }
