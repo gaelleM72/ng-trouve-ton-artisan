7e6bcb63ef7e5ab724bb07c2cb08756b619f164f
gaelleM72 <141839974+gaelleM72@users.noreply.github.com>
2024-06-18 17:30:18 +0200

    batiment

--- src/app/batiment/batiment.component.ts
+++ src/app/batiment/batiment.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ARTISANS } from '../artisan/artisan-list';
+import { Artisan } from '../artisan/artisans';
 
 @Component({
   selector: 'app-batiment',
   templateUrl: './batiment.component.html',
   styleUrl: './batiment.component.scss'
 })
-export class BatimentComponent {
+export class BatimentComponent implements OnInit {
+ 
+  artisanList: Artisan[] = ARTISANS;
+  artisanSelected: Artisan|undefined;
+
+  ngOnInit() {
+    this.artisanList = ARTISANS.filter((Artisan: { category:string }) => Artisan.category === "Bâtiment");
+  }
 
 }
